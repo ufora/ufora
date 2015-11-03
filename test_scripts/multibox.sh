1ed3cad452552ee1863d52b3de79e279a44e8336
Ronen Hilewicz <ronen@ufora.com>
2015-11-03 13:17:04 -0500

    [#62] Increasing the multibox test timeout (for real)
    The prior commit used the wrong command line argument (`--timeout`
    instead of `-timeout`) and had no effect.

--- test_scripts/multibox.sh
+++ test_scripts/multibox.sh
@@ -5,7 +5,7 @@ fi
 . $WORKSPACE/test_scripts/_funcs.sh
 
 TEST_NAME=multibox
-TEST_ARGS="-multibox --logging=info --timeout 600 $*"
+TEST_ARGS="-multibox --logging=info -timeout 600 $*"
 export UFORA_CONFIG_FILE=$WORKSPACE/test_scripts/multibox/test.config
 
 run_test
