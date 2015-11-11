23206e5e40d1e70798e2f5480c72af7c603b4106
Tom Peters <thomas.d.peters@gmail.com>
2015-11-11 14:22:48 -0500

    minor, general code cleanup:
    * remove an unused import
    * remove a dead comment
    * fixup whitespace in PyDict

--- packages/python/pyfora/typeConverters/Numpy.py
+++ packages/python/pyfora/typeConverters/Numpy.py
@@ -12,7 +12,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import pyfora.PyObjectNodes as PyObjectNodes
 import pyfora.PureImplementationMapping as PureImplementationMapping
 import numpy as np
 import pyfora.BuiltinPureImplementationMappings as BuiltinPureImplementationMappings
