5ebcd1b9ff3651447de123c3fcd333306e19e8c5
Tom Peters <thomas.d.peters@gmail.com>
2016-10-31 14:51:22 -0400

    fixup PyObjectUtils::format_exc

--- packages/python/pyfora/src/PyObjectUtils.cpp
+++ packages/python/pyfora/src/PyObjectUtils.cpp
@@ -98,37 +98,140 @@ std::string PyObjectUtils::std_string(PyObject* string)
 
 std::string PyObjectUtils::format_exc()
     {
-    PyObject* traceback_module = PyImport_ImportModule("traceback");
-    if (traceback_module == NULL) {
-        PyErr_Print();
-        throw std::runtime_error("error importing traceback module");
+    PyObject * exception = NULL, * v = NULL, * tb = NULL;
+    PyErr_Fetch(&exception, &v, &tb);
+    if (exception == NULL) {
+        return "<no exception>";
+        }
+    PyErr_NormalizeException(&exception, &v, &tb);
+
+    PyObject* typeString = PyObject_Str(exception);
+    Py_DECREF(exception);
+    if (typeString == NULL) {
+        Py_XDECREF(tb);
+        Py_XDECREF(v);
+        return "<INTERNAL ERROR: couldn't get typeString in PyObjectUtils::format_exc>";
+        }
+    if (not PyString_Check(typeString)) {
+        Py_DECREF(typeString);
+        Py_XDECREF(tb);
+        Py_XDECREF(v);
+        return "<INTERNAL ERROR: str(exception) didn't return a string in" 
+            " PyObjectUtils::format_exc";
         }
 
-    PyObject* format_exc_fun = PyObject_GetAttrString(
-        traceback_module,
-        "format_exc");
-    if (format_exc_fun == NULL) {
-        PyErr_Print();
-        throw std::runtime_error("error getting traceback.format_exc function");
+    if (v == NULL) {
+        Py_DECREF(typeString);
+        Py_XDECREF(tb);
+        return "<v unexpectedly none in PyObjectUtils::format_exc>";
+        }
+    PyObject* valueString = PyObject_Str(v);
+    Py_DECREF(v);
+    if (valueString == NULL) {
+        Py_DECREF(typeString);
+        Py_XDECREF(tb);
+        return "<INTERNAL ERROR: couldn't get value string>";
+        }
+    if (not PyString_Check(valueString)) {
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);
+        Py_XDECREF(tb);
+        return "<INTERNAL ERROR: str(v) didn't return a string!>";
         }
 
-    PyObject* tb_str = PyObject_CallFunctionObjArgs(format_exc_fun, NULL);
-    if (tb_str == NULL) {
-        PyErr_Print();
-        throw std::runtime_error("error calling traceback.format_exc");
+    PyObject* cStringIOModule = PyImport_ImportModule("cStringIO");
+    if (cStringIOModule == NULL) {
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);
+        Py_XDECREF(tb);
+        throw std::runtime_error(
+            "py error importing cStringIO module: " + 
+            PyObjectUtils::exc_string()
+            );
+        }
+    
+    PyObject* StringIOClass = PyObject_GetAttrString(
+        cStringIOModule,
+        "StringIO");
+    Py_DECREF(cStringIOModule);
+    if (StringIOClass == NULL) {
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);
+        Py_XDECREF(tb);
+        throw std::runtime_error(
+            "py error getting StringIO member on cStringIO in PyObjectUtils::format_exc: " +
+            PyObjectUtils::exc_string()
+            );
         }
 
-    if (not PyString_Check(tb_str)) {
-        throw std::runtime_error("expected a string");
+    PyObject* stringIO = PyObject_CallFunctionObjArgs(StringIOClass, NULL);
+    Py_DECREF(StringIOClass);
+    if (stringIO == NULL) {
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);
+        Py_XDECREF(tb);
+        throw std::runtime_error(
+            "py error calling StringIO in PyObjectUtils::format_exc: " + 
+            PyObjectUtils::exc_string()
+            );
         }
 
-    std::string tr = PyObjectUtils::std_string(tb_str);
+    if (tb == NULL) {
+        Py_DECREF(stringIO);
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);        
+        throw std::runtime_error(
+            "<tb unexpectedly NULL in PyObjectUtils::format_exc>"
+            );
+        }
+    int retcode = PyTraceBack_Print(tb, stringIO);
+    Py_DECREF(tb);
+    if (retcode < 0) {
+        Py_DECREF(stringIO);
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);        
+        throw std::runtime_error(
+            "error calling PyTraceBack_Print in PyObjectUtils::format_exc: " +
+            PyObjectUtils::exc_string()
+            );
+        }
+    PyObject* tb_string = PyObject_CallMethod(
+        stringIO,
+        const_cast<char*>("getvalue"),
+        const_cast<char*>("()")
+        );
+    Py_DECREF(stringIO);
+    if (tb_string == NULL) {
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);        
+        throw std::runtime_error(
+            "py error calling getvalue on a StringIO in PyObjectUtils::format_exc:" +
+            PyObjectUtils::exc_string()
+            );
+        }
+    if (not PyString_Check(tb_string)) {
+        Py_DECREF(tb_string);
+        Py_DECREF(valueString);
+        Py_DECREF(typeString);        
+        throw std::runtime_error("<expected getvalue to return a string>");
+        }
 
-    Py_DECREF(tb_str);
-    Py_DECREF(format_exc_fun);
-    Py_DECREF(traceback_module);
+    std::ostringstream oss;
 
-    return tr;
+    oss.write(PyString_AS_STRING(typeString), PyString_GET_SIZE(typeString));
+    oss << ": ";
+    oss.write(PyString_AS_STRING(valueString), PyString_GET_SIZE(valueString));
+
+    Py_DECREF(valueString);
+    Py_DECREF(typeString);
+
+    oss << "\n";
+    
+    oss.write(PyString_AS_STRING(tb_string), PyString_GET_SIZE(tb_string));
+
+    Py_DECREF(tb_string);
+
+    return oss.str();
     }
 
 
@@ -152,36 +255,29 @@ std::string PyObjectUtils::exc_string()
         return "<no exception>";
         }
     PyErr_NormalizeException(&exception, &v, &tb);
+    Py_XDECREF(tb);
 
     PyObject* typeString = PyObject_Str(exception);
+    Py_DECREF(exception);
     if (typeString == NULL) {
-        Py_DECREF(exception);
         Py_XDECREF(v);
-        Py_XDECREF(tb);
         return "<INTERNAL ERROR: couldn't get typeString>";
         }
     if (not PyString_Check(typeString)) {
         Py_DECREF(typeString);
-        Py_DECREF(exception);
         Py_XDECREF(v);
-        Py_XDECREF(tb);
         return "<INTERNAL ERROR: str(exception) didn't return a string!>";
         }
 
     PyObject* valueString = PyObject_Str(v);
+    Py_XDECREF(v);
     if (valueString == NULL) {
         Py_DECREF(typeString);
-        Py_DECREF(exception);
-        Py_XDECREF(v);
-        Py_XDECREF(tb);
         return "<INTERNAL ERROR: couldn't get value string>";
         }
     if (not PyString_Check(valueString)) {
         Py_DECREF(valueString);
         Py_DECREF(typeString);
-        Py_DECREF(exception);
-        Py_XDECREF(v);
-        Py_XDECREF(tb);
         return "<INTERNAL ERROR: str(v) didn't return a string!>";
         }
 
@@ -193,9 +289,6 @@ std::string PyObjectUtils::exc_string()
 
     Py_DECREF(valueString);
     Py_DECREF(typeString);
-    Py_DECREF(exception);
-    Py_XDECREF(v);
-    Py_XDECREF(tb);
 
     return oss.str();
     }
