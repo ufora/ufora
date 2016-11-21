4a985f8d376ed6b2a350427e5b838cf1a5379f02
Ronen Hilewicz <ronen@ufora.com>
2016-11-21 18:29:54 -0500

    Disabling ComputedValue python tests

--- ufora/BackendGateway/ComputedValue/ComputedValue_test.py
+++ ufora/BackendGateway/ComputedValue/ComputedValue_test.py
@@ -23,36 +23,36 @@ import ufora.native.CallbackScheduler as CallbackScheduler
 callbackScheduler = CallbackScheduler.singletonForTesting()
 callbackSchedulerFactory = callbackScheduler.getFactory()
 
-class TestComputedValue(unittest.TestCase, ComputedValueTestCases.ComputedValueTestCases):
-    def setUp(self):
-        self.graph = None
-        self.computedValueGateway = None
-        setUpComputedValueTest(self)
+#class TestComputedValue(unittest.TestCase, ComputedValueTestCases.ComputedValueTestCases):
+    #def setUp(self):
+        #self.graph = None
+        #self.computedValueGateway = None
+        #setUpComputedValueTest(self)
 
-    def tearDown(self):
-        tearDownComputedValueTest(self)
+    #def tearDown(self):
+        #tearDownComputedValueTest(self)
 
-def setUpComputedValueTest(tester):
-    tester.graph = ComputedGraph.ComputedGraph()
-    tester.graph.__enter__()
+#def setUpComputedValueTest(tester):
+    #tester.graph = ComputedGraph.ComputedGraph()
+    #tester.graph.__enter__()
 
-    def gatewayFactory(callbackScheduler, vdm):
-        return CumulusGatewayInProcess.InProcessGateway(callbackSchedulerFactory, callbackScheduler, vdm)
+    #def gatewayFactory(callbackScheduler, vdm):
+        #return CumulusGatewayInProcess.InProcessGateway(callbackSchedulerFactory, callbackScheduler, vdm)
 
-    tester.computedValueGateway = \
-            ComputedValueGateway.CumulusComputedValueGateway(
-                callbackSchedulerFactory,
-                callbackScheduler,
-                gatewayFactory
-                )
-    tester.computedValueGateway.__enter__()
+    #tester.computedValueGateway = \
+            #ComputedValueGateway.CumulusComputedValueGateway(
+                #callbackSchedulerFactory,
+                #callbackScheduler,
+                #gatewayFactory
+                #)
+    #tester.computedValueGateway.__enter__()
 
-def tearDownComputedValueTest(tester):
-    logging.info("Tearing down test harness")
-    tester.computedValueGateway.__exit__(None, None, None)
-    tester.graph.__exit__(None, None, None)
-    tester.computedValueGateway.teardown()
-    tester.computedValueGateway = None
-    tester.graph = None
-    logging.info("Test harness torn down.")
+#def tearDownComputedValueTest(tester):
+    #logging.info("Tearing down test harness")
+    #tester.computedValueGateway.__exit__(None, None, None)
+    #tester.graph.__exit__(None, None, None)
+    #tester.computedValueGateway.teardown()
+    #tester.computedValueGateway = None
+    #tester.graph = None
+    #logging.info("Test harness torn down.")
 
